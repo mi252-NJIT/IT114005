449b343cbfad880a3221d5492b7c9265d684afd4
mi252-NJIT <70656699+mi252-NJIT@users.noreply.github.com>
2020-09-12 12:07:34 -0400

    Add files via upload

---
+++ Homework.java
@@ -0,0 +1,32 @@
+// Michael Imbro
+// IT 114 HW 1
+// 9/14/2020
+
+public class Homework {
+	public static void main(String[] args) {
+		// 1. Create a variable that holds a name
+
+		String name = "Michael";
+
+		// 2. Output the name to the console
+
+		System.out.println(name);
+
+		// 3. Create a variable that holds an age
+
+		int age = 22;
+
+		// 4. Output the age to the console
+
+		System.out.println(age);
+
+		// 5. Create a variable that holds a decimal
+
+		float decimal = 1.2f;
+
+		// 6. Output the variable
+
+		System.out.println(decimal);
+
+	}
+}
