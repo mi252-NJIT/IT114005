7b0da378c9bbbd43dfda7cda65b214b3a404aa87
= <mi252@njit.edu>
2020-10-16 17:44:41 -0400

    Part 5 attempt 2

--- project/SocketClient.java
+++ project/SocketClient.java
@@ -1,5 +1,5 @@
 
-import java.io.IOException;
+import java.io.IOException; 
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
@@ -8,10 +8,11 @@ import java.util.Scanner;
 
 import utils.Debug;
 
-public class SocketClient {
+public class SocketClient implements AutoCloseable {
 	private Socket server;
 	private Thread inputThread;
 	private Thread fromServerThread;
+	private String clientName;
 	
 	public void connect(String address, int port) {
 		try {
@@ -28,65 +29,140 @@ public class SocketClient {
 		
 	}
 	
-	private void listenForKeyboard(Scanner si, ObjectOutputStream out) {
-		inputThread = new Thread() {
-			@Override
-			public void run() {
-				try {
-					while (!server.isClosed()) {
-						Debug.log("Waiting for input");
-						String line = si.nextLine();
-						
-						if (!"quit".equalsIgnoreCase(line) && line != null) {
-							out.writeObject(line);
-						} else {
-							Debug.log("Stopping input thread");
-							out.writeObject("bye");
-							break;
-						}
-						try {
-							sleep(50);
-						} catch (Exception e) {
-							Debug.log("Problem sleeping thread");
-							e.printStackTrace();
-						}
-					}
-				} catch (Exception e) {
-					e.printStackTrace();
-				} finally {
-					close();
-					Debug.log("Stopped listening to console input");
-				}
-			}
-			
-		};
-		inputThread.start();
-	}
+    private void readUsername(Scanner si) {
+    	System.out.println("Please enter a username and press enter...");
+    	clientName = si.nextLine();
+    }
+    
+    private Payload buildMessage(String message) {
+    	Payload payload = new Payload();
+    	payload.setPayloadType(PayloadType.MESSAGE);
+    	payload.setClientName(clientName);
+    	payload.setMessage(message);
+    	return payload;
+    }
+    
+    private Payload buildConnectionStatus(String name, boolean isConnect) {
+		Payload payload = new Payload();
+		if (isConnect) {
+		    payload.setPayloadType(PayloadType.CONNECT);
+		}
+		else {
+		    payload.setPayloadType(PayloadType.DISCONNECT);
+		}
+		payload.setClientName(name);
+		return payload;
+    }
+    
+    private void sendPayload(Payload p, ObjectOutputStream out) {
+		try {
+		    out.writeObject(p);
+		}
+		catch (IOException e) {
+		    // TODO Auto-generated catch block
+		    e.printStackTrace();
+		}
+    }
+    
+    
+    private void listenForKeyboard(Scanner si, ObjectOutputStream out) {
+    	if (inputThread != null) {
+    	    Debug.log("Input Listener is likely already running");
+    	    return;
+    	}
+    	// Thread to listen for keyboard input so main thread isn't blocked
+    	inputThread = new Thread() {
+    	    @Override
+    	    public void run() {
+	    		try {
+	    		    readUsername(si);
+	    		    sendPayload(buildConnectionStatus(clientName, true), out);
+	
+	    		    while (!server.isClosed()) {
+		    			Debug.log("Waiting for input");
+		    			String line = si.nextLine();// this line causes a problem due to blocking IO when the server
+		    						    // terminates
+		    			if (!"quit".equalsIgnoreCase(line) && line != null) {
+		    			    // grab line and write it to the stream
+		    			    sendPayload(buildMessage(line), out);
+		    			}
+		    			else {
+		    			    Debug.log("Stopping input thread");
+		    			    // we're quitting so tell server we disconnected so it can broadcast
+		    			    sendPayload(buildConnectionStatus(clientName, false), out);
+		    			    break;
+		    			}
+		    			try {
+		    			    sleep(50);
+		    			}
+		    			catch (Exception e) {
+		    			    Debug.log("Problem sleeping thread");
+		    			    e.printStackTrace();
+		    			}
+	    		    }
+	    		}
+	    		catch (Exception e) {
+	    		    e.printStackTrace();
+	    		}
+	    		finally {
+	    		    close();
+	    		    Debug.log("Stopped listening to console input");
+	    		}
+    	    }
+    	};
+    	inputThread.start();// start the thread
+    }
 	
 	
-	private void listenForServerMessage(ObjectInputStream in) {
+    private void listenForServerMessage(ObjectInputStream in) {
+		if (fromServerThread != null) {
+		    Debug.log("Server Listener is likely already running");
+		    return;
+		}
+		// Thread to listen for responses from server so it doesn't block main thread
 		fromServerThread = new Thread() {
-			@Override
-			public void run() {
+		    @Override
+		    public void run() {
 				try {
-					String fromServer;
-					while (!server.isClosed() && (fromServer = (String) in.readObject()) != null) {
-						System.out.println(fromServer);
-					}
-				} catch (Exception e) {
-					if (!server.isClosed()) {
-						e.printStackTrace();
-						Debug.log("Connection closed");
-					} else {
-						Debug.log("Connection closed");
-					}
-				} finally {
-					close();
-					Debug.log("Stopped Listening to server input");
+				    Payload fromServer;
+				    // while we're connected, listen for Payloads from server
+				    while (!server.isClosed() && (fromServer = (Payload) in.readObject()) != null) {
+					processPayload(fromServer);
+				    }
 				}
-			}
+				catch (Exception e) {
+				    if (!server.isClosed()) {
+					e.printStackTrace();
+					Debug.log("Server closed connection");
+				    }
+				    else {
+					Debug.log("Connection closed");
+				    }
+				}
+				finally {
+				    close();
+				    Debug.log("Stopped listening to server input");
+				}
+		    }
 		};
-		fromServerThread.start();
+		fromServerThread.start();// start the thread
+    }
+    
+    private void processPayload(Payload p) {
+		switch (p.getPayloadType()) {
+			case CONNECT:
+			    System.out.println(p.getClientName() + ": " + p.getMessage());
+			    break;
+			case DISCONNECT:
+			    System.out.println(p.getClientName() + ": " + p.getMessage());
+			    break;
+			case MESSAGE:
+			    System.out.println(p.getClientName() + ": " + p.getMessage());
+			    break;
+			default:
+			    Debug.log("Unhandled payload on client: " + p);
+			    break;
+		}
 	}
 	
 	
@@ -116,41 +192,37 @@ public class SocketClient {
 		}
 	}
 	
-	
-	private void close() {
-		if (server != null) {
-			try {
+	@Override
+    public void close() {
+		if (server != null && !server.isClosed()) {
+		    try {
 				server.close();
-				Debug.log("Closed socket");;
-			}
-			catch (IOException e) {
+				Debug.log("Closed socket");
+		    }
+		    catch (IOException e) {
 				e.printStackTrace();
-			}
+		    }
 		}
-	}
+    }
 	
-	public static void main(String[] args) {
-		SocketClient client = new SocketClient();
+    public static void main(String[] args) {
+	
 		int port = -1;
-		
 		try {
-			port = Integer.parseInt(args[0]);
+		    // not safe but try-catch will get it
+		    port = Integer.parseInt(args[0]);
 		}
 		catch (Exception e) {
-			Debug.log("Invalid port");
+		    Debug.log("Invalid port");
 		}
-		
-		if(port == -1) {
-			return;
+		if (port > -1) {
+		    try (SocketClient client = new SocketClient();) {
+				client.connect("127.0.0.1", port);
+				client.start();
+		    }
+		    catch (IOException e) {
+				e.printStackTrace();
+		    }
 		}
-		
-		client.connect("127.0.0.1", port);
-		try {
-			client.start();
-		}
-		catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-	}
+    }
 }
